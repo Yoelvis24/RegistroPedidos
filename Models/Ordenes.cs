d738496833a90945a6981852ff276ff957201500
Yoelvis Perez <yoelvis.perez.20017@gmail.com>
2021-10-17 23:51:01 -0400

    Add project files.

---
+++ Models/Ordenes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPedidos.Models
+{
+    public class Ordenes
+    {
+        [Key]
+        public int OrdenId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int SuplidorId { get; set; }
+        public float Monto { get; set; }
+
+        public virtual ICollection<OrdenesDetalle> OrdenesDetalle { get; set; }
+
+        public Ordenes()
+        {
+            OrdenId = 0;
+            Fecha = DateTime.Now;
+            SuplidorId = 0;
+            Monto = 0;
+            OrdenesDetalle = new HashSet<OrdenesDetalle>();
+        }
+    }
+}
---
+++ Models/OrdenesDetalle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPedidos.Models
+{
+    public class OrdenesDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+        public int OrdenId { get; set; }
+        public Ordenes Orden { get; set; }
+        public int ProductoId { get; set; }
+        public Productos Producto { get; set; }
+        public float Cantidad { get; set; }
+        public float Costo { get; set; }
+
+        public OrdenesDetalle()
+        {
+            DetalleId = 0;
+            OrdenId = 0;
+            ProductoId = 0;
+            Cantidad = 0;
+            Costo = 0;
+        }
+    }
+}
